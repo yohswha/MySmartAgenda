730fbfb88f7a1c5fbb071b49e49306924588b463
YohswhaTheThinker <49953086+yohswha@users.noreply.github.com>
2024-07-12 14:36:00 -0400

    Update IUserRepository.cs

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -6,7 +6,10 @@ namespace MySmartAgenda.Interfaces
 	public interface IUserRepository
 	{
 		ICollection<User> GetUsers();
-		
+		User GetSingleUser(int Id);
+		User Put( int Id)
+		User UpdateUser( int Id);
+		User DeleteUser(int Id);	
 	}
 }
 
